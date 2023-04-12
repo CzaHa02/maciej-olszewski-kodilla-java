d22b922a387f09bc69125fcd4bd37b9368afe219
Maciej Olszewski <maciek.ol@op.pl>
2023-04-12 12:01:54 +0200

    6.1 Added kodilla-testing module

---
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -0,0 +1,8 @@
+package com.kodilla.testing;
+
+public class TestingMain {
+    public static void main(String[]args){
+        System.out.println("moduł 6. Wprowadzenie do testowania oprogramowania");
+
+    }
+}
